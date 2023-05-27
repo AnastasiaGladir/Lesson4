a775893e81b79acbcd25eae241f7ee12ae80ee21
Anastasia <nasti.listic@mail.ru>
2023-05-27 13:04:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/gladirap/lesson4/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.mirea.gladirap.lesson4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.mirea.gladirap.lesson4.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected	void	onCreate(Bundle	savedInstanceState)	{
+        super.onCreate(savedInstanceState);
+        binding	=	ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
